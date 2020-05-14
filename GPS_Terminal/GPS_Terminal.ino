1523f0e454b961b3f420760d27a7b82bf8202a42
Hansen0314 <595355940@qq.com>
2020-05-14 17:27:33 +0800

    update:finish GPS Terminal v1.0

--- GPS_Terminal/GPS_Terminal.ino
+++ GPS_Terminal/GPS_Terminal.ino
@@ -1,82 +1,44 @@
-#include <TinyGPS++.h>
 #include <SoftwareSerial.h>
- 
+#include <TFT_eSPI.h>
+
 static const int RXPin = 0, TXPin = 1;
 static const uint32_t GPSBaud = 9600;
- 
-// The TinyGPS++ object
-TinyGPSPlus gps;
- 
+
 // The serial connection to the GPS device
 SoftwareSerial ss(RXPin, TXPin);
  
+TFT_eSPI tft;
+TFT_eSprite spr = TFT_eSprite(&tft);  // Sprite 
+
 void setup()
 {
   Serial.begin(9600);
   ss.begin(GPSBaud);
+  tft.begin();
+  tft.setRotation(3);
+  spr.createSprite(tft.width(),tft.height()); 
 }
- 
 void loop()
 {
-  // This sketch displays information every time a new sentence is correctly encoded.
-  while (ss.available() > 0)
-    if (gps.encode(ss.read()))
-      displayInfo();
- 
-  if (millis() > 5000 && gps.charsProcessed() < 10)
-  {
-    Serial.println(F("No GPS detected: check wiring."));
-    while(true);
-  }
-}
- 
-void displayInfo()
-{
-  Serial.print(F("Location: ")); 
-  if (gps.location.isValid())
-  {
-    Serial.print(gps.location.lat(), 6);
-    Serial.print(F(","));
-    Serial.print(gps.location.lng(), 6);
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
- 
-  Serial.print(F("  Date/Time: "));
-  if (gps.date.isValid())
-  {
-    Serial.print(gps.date.month());
-    Serial.print(F("/"));
-    Serial.print(gps.date.day());
-    Serial.print(F("/"));
-    Serial.print(gps.date.year());
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
- 
-  Serial.print(F(" "));
-  if (gps.time.isValid())
-  {
-    if (gps.time.hour() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.hour());
-    Serial.print(F(":"));
-    if (gps.time.minute() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.minute());
-    Serial.print(F(":"));
-    if (gps.time.second() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.second());
-    Serial.print(F("."));
-    if (gps.time.centisecond() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.centisecond());
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
- 
-  Serial.println();
+  
+    spr.fillSprite(TFT_BLACK);
+    spr.setCursor(0,5,2);
+    spr.println("The GPS raw data"); 
+    spr.setCursor(0,20,2);
+    while(ss.available())                     // if date is coming from software serial port ==> data is coming from ss shield
+    {              
+        uint8_t reciver = ss.read();
+        Serial.write(reciver);
+        spr.write(reciver);
+        int basetime = millis();
+        while (!ss.available());
+        int timecost = millis() - basetime;
+        if(timecost > 709){
+            break;
+        } 
+    }
+    if (Serial.available())                 // if data is available on hardware serial port ==> data is coming from PC or notebook
+        ss.write(Serial.read());        // write it to the ss shield
+    spr.pushSprite(0, 0);
+    Serial.print("----------------------------------------------------------\r\n");
 }
